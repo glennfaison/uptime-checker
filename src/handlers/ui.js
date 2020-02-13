e85e83258bb32aafd29451d7d818cf0280b48ed7
glennfaison <glennfaison@gmail.com>
2020-02-13 17:19:44 +0100

    Add a basic templating engine and serve templates on demand

--- src/handlers/ui.js
+++ src/handlers/ui.js
@@ -6,12 +6,10 @@ const uiHandler = {};
 
 
 uiHandler.index = async (req, res) => {
-  
   let str = await templatr.getTemplate("index.html", { appName: "Checks App", heading: "My heading"});
   res.setHeader("Content-Type", "text/html");
   res.writeHead(200, "OK");
   return res.end(str);
-  
 };
 
 
