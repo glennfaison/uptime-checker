230aa66b757cc79521850362917ac50ca1745577
glennfaison <glennfaison@gmail.com>
2020-02-14 01:26:44 +0100

    Fix templatr engine

--- src/handlers/ui.js
+++ src/handlers/ui.js
@@ -7,7 +7,7 @@ const uiHandler = {};
 
 
 uiHandler.index = async (req, res) => {
-  let str = templatr.renderTemplate("index.html");
+  let str = templatr.renderTemplate("index.html", { pageTitle: "Home", appName: "MyApp", body: { className: "index" } });
   res.setHeader("Content-Type", "text/html");
   res.writeHead(200, "OK");
   return res.end(str);
