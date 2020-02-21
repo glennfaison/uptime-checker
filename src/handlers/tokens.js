67918477f288ecc18f84a1f27223409b6a030cfe
glennfaison <glennfaison@gmail.com>
2020-02-21 11:43:58 +0100

    Use performance benchmarks to profile performance of token creation api

--- src/handlers/tokens.js
+++ src/handlers/tokens.js
@@ -1,3 +1,6 @@
+const { performance, PerformanceObserver } = require("perf_hooks");
+const debuglog = require("util").debuglog("performance");
+
 const db = require("../db");
 const helpers = require("../helpers");
 
@@ -9,26 +12,52 @@ const tokenController = {};
  * @returns
  */
 tokenController.post = async (req, res) => {
+  // log entries by type
+  const obs = new PerformanceObserver(items => {
+    items.getEntries().forEach(i => {
+      debuglog(`\x1b[33m%s\x1b[0m`, `${i.name}: ${i.duration}`);
+    });
+  });
+  obs.observe({ entryTypes: ["measure"] });
+
+  performance.mark("entered function");
   const phone = req.body.phone ? req.body.phone.trim() : undefined;
   const password = req.body.password ? req.body.password.trim() : undefined;
+  performance.mark("inputs validated");
 
   if (!phone || !password) {
     return res.setStatus(400).json({ error: `Missing required fields` });
   }
+  performance.mark("beginning user lookup");
   let user = await db.read("users", phone).catch(e => user = null);
+  performance.mark("user lookup complete");
   if (!user) {
     return res.setStatus(400).json({ error: "Could not find the specified user" });
   }
   // check that password hash matches saved hashedPassword
+  performance.mark("beginning password hashing");
   const hashedPassword = helpers.hash(password);
+  performance.mark("password hashing complete");
   if (hashedPassword !== user.hashedPassword) {
     return res.setStatus(400).json({ error: "Specified password does not match stored password" });
   }
   // Create a new token and set a 1-hour expiration date
+  performance.mark("creating data for token");
   const expires = Date.now() + 60 * 60 * 1000;
   const tokenId = helpers.createRandomString(20);
   const tokenObj = { phone, expires, tokenId };
+  performance.mark("beginning storing token");
   let created = await db.create("tokens", tokenId, tokenObj).catch(e => created = false);
+  performance.mark("storing token complete");
+
+  // Gather all measurements
+  performance.measure('Beginning to end', 'entered function', 'storing token complete');
+  performance.measure('Validating user inputs', 'entered function', 'inputs validated');
+  performance.measure('User lookup', 'beginning user lookup', 'user lookup complete');
+  performance.measure('Password hashing', 'beginning password hashing', 'password hashing complete');
+  performance.measure('Token data creation','creating data for token', 'beginning storing token');
+  performance.measure('Token storing','beginning storing token', 'storing token complete');
+
   if (!created) {
     return res.setStatus(500).json({ error: "Could not create new token" });
   }
